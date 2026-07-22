f04e2d2afd2de9daca444a14cf025a407cec1900
jorgeLinares <jorgelinaresrrs306@hotmail.com>
2026-07-22 11:13:59 -0500

    refactor!: migrate backend from Express to NestJS at root level

--- config/db.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'mongoose';
-import logger from './logger.js';
-
-connect(ENV.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => logger.info('Conexión exitosa a MongoDB'))
-  .catch(err => logger.error('Error al conectar a MongoDB:', err));
--- routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import protect from '../middlewares/authMiddleware.js';
-import { getDashboardStats } from '../controllers/statsController.js';
-
-const router = express.Router();
-
-router.get('/', protect, getDashboardStats);
-
-export default router;
